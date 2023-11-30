94f7e7edd192a8693e6a261120c0ed8fc7d072c1
nuha <nuhaahmad744@gmail.com>
2023-11-30 12:36:57 +0300

    solve importing if fields are empty

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -24,29 +24,41 @@ class UsersImport implements ToModel
             $this->firstRow = false;
             return null;
         }
+
         $statusName = $row['6'];
         $cityName = $row['3'];
         $superviserName = $row['7'];
+
         $status = Status::where('name', $statusName)->first();
         $city = City::where('city_name', $cityName)->first();
         $superviser = Superviser::where('name', $superviserName)->first();
 
-        return new Donation([
+        $data = [
             'name' => $row['0'],
             'address' => $row['1'],
             'familyMembers' => $row['2'],
-            'city_id' => $city->city_id,
             'birthdate' => $row['4'],
             'amount' => $row['5'],
-            'status_id' => $status->status_id,
-            'superviser_id' => $superviser->superviser_id,
             'Tel1' => $row['8'],
             'Tel2' => $row['9'],
             'note' => $row['10'],
             'active' => $row['11'],
+        ];
 
-            // ...other fields
-        ]);
-        // return Redirect::to('/nova/resources/donations');
+        // Check if Status, City, and Superviser are found or not
+        if ($status) {
+            $data['status_id'] = $status->status_id;
+        }
+
+        if ($city) {
+            $data['city_id'] = $city->city_id;
+        }
+
+        if ($superviser) {
+            $data['superviser_id'] = $superviser->superviser_id;
+        }
+
+        // Create the Donation model instance
+        return new Donation($data);
     }
 }
