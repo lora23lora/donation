f86a489dbb87706c5504df7a3a8ec811316cf317
nuha <nuhaahmad744@gmail.com>
2024-01-22 12:01:01 +0300

    fixed BeneficiariesImport

--- app/Imports/BeneficiariesImport.php
+++ app/Imports/BeneficiariesImport.php
@@ -6,11 +6,11 @@ use App\Models\Beneficiary;
 use App\Models\City;
 use App\Models\Superviser;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class BeneficiariesImport implements ToModel
+class BeneficiariesImport implements ToModel, WithStartRow
 {
 
-    private $firstRow = true;
     /**
     * @param array $row
     *
@@ -19,10 +19,7 @@ class BeneficiariesImport implements ToModel
 
     public function model(array $row)
     {
-        if ($this->firstRow) {
-            $this->firstRow = false;
-            return null;
-        }
+
 
         $cityName = $row['1'];
         $superviserName = $row['6'];
@@ -31,15 +28,9 @@ class BeneficiariesImport implements ToModel
         $city = City::where('city_name', $cityName)->first();
         $superviser = Superviser::where('name', $superviserName)->first();
 
-        if ($city) {
-            $data['city_id'] = $city->city_id;
-        }
 
-        if ($superviser) {
-            $data['superviser_id'] = $superviser->superviser_id;
-        }
 
-        return new Beneficiary([
+        $beneficiary = new Beneficiary([
             'name' => $row['0'],
             'address' => $row['2'],
             'birthdate' => $row['3'],
@@ -50,5 +41,15 @@ class BeneficiariesImport implements ToModel
             'active' => $row['9'],
             'note' => $row['10'],
         ]);
+        
+        $beneficiary->city_id = $city ? $city->city_id : null;
+
+        // Check if the supervisor is not null before accessing the supervisor_id
+        $beneficiary->superviser_id = $superviser ? $superviser->superviser_id : null;
+
+        return $beneficiary;
+    }
+    public function startRow():int{
+        return 2;
     }
 }
